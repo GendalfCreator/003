4ed29ada25dba0622c11f3e38187f45b06352854
Alex <frstfng@gmail.com>
2021-02-03 21:43:32 +0300

    Все задания выполнены

--- main.cpp
+++ main.cpp
@@ -1,9 +1,40 @@
 #include <iostream>
-
+#include <cmath>
+#include "params.h"
 using namespace std;
 
 int main()
 {
-    cout << "Hello World!" << endl;
+    //01. Вычисление выражения и вывод на экран
+    const int a = 2, b =3, c = 7, d = 11;
+    float result;
+
+    result = a * (b + ((float)c / d));
+
+    cout << result << endl;
+
+    //02. Использование тернарного оператора
+    int dano = 127;
+    int difference;
+
+    difference = (dano > 21) ? 2 * (21 - dano) : 21 - dano;
+
+    cout << abs(difference) << endl;
+
+    //03. Инициализация переменных в стороннем файле (params.h)
+    const extern int x, y, z, q;
+    float result00;
+
+    result00 = x * (y + ((float)z / q));
+
+    cout << result00 << endl;
+
+    //04. Вывод значения ячейки куба по указателю
+    int cube[3][3][3] = {};
+    cube[1][1][1] = 143; //Для отладки
+    int *cube_element;
+    cube_element = &cube[1][1][1];
+
+    cout << *cube_element << endl;
     return 0;
 }
---
+++ params.h
@@ -0,0 +1,7 @@
+#ifndef PARAMS_H
+#define PARAMS_H
+
+#endif // PARAMS_H
+//Инициализация переменных для задания 03
+const int x = 2, y = 3, z = 4, q = 7;
+
